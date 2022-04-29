9191e246ff5bcea5e6370bb79a5c58b5b66a5b95
payamrwd <payamrwd@gmail.com>
2022-04-29 20:54:04 +0430

    working on layout (1)

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,1 +1,28 @@
+import Head from 'next/head';
+import { ReactNode } from 'react';
 import styles from '../styles/layout.module.scss';
+
+type Props = {
+  children: ReactNode;
+};
+
+function Layout({ children }: Props) {
+  return (
+    <div className={styles.wrapper}>
+      <Head>
+        <title>Next.js TS Test App</title>
+        <meta name="description" content="A Test Next.js App by PayamRWD!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Navigation!</h1>
+        {children}
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/home.module.scss';
+import Layout from '../components/layout';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.wrapper}>
-      <Head>
-        <title>Next.js TS Test App</title>
-        <meta name="description" content="A Test Next.js App by PayamRWD!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>Hi Next.js</h1>
-      </main>
-
-      <footer></footer>
-    </div>
+    <Layout>
+      <h2>index page!</h2>
+    </Layout>
   );
 };
 
