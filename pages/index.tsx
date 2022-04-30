8e1077ad646feb69ece7644b99f0888d8a535451
payamrwd <payamrwd@gmail.com>
2022-04-30 17:14:09 +0430

    working on layout (2)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
 import Layout from '../components/layout';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <Layout>
+      <Head>
+        <title>index page title</title>
+      </Head>
       <h2>index page!</h2>
     </Layout>
   );
