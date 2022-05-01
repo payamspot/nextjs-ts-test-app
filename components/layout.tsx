98b400e151c4b51e2d1e1afe2e814b5aeb011d3b
payamrwd <payamrwd@gmail.com>
2022-05-01 10:34:36 +0430

    added roboto fonts to layout.tsx to be used by mui

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
 import { ReactNode } from 'react';
 import styles from '../styles/layout.module.scss';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 type Props = {
   children: ReactNode;
