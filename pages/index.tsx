ad9b3e4295718169af969870c4f42e54b9effcda
payamrwd <payamrwd@gmail.com>
2022-05-03 09:57:26 +0430

    loading index products using getStaticProps (1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,31 @@ import Layout from '../components/layout';
 import Head from 'next/head';
 import styles from '../styles/index.module.scss';
 
+type Product = {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+};
+
 const Home: NextPage = () => {
   return (
     <Layout>
       <Head>
         <title>index page title</title>
       </Head>
-      <h2 className={styles.blue}>index page!</h2>
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3333/products');
+  const products: Product[] = await res.json();
+  return {
+    props: {
+      products,
+    },
+  };
+}
 
 export default Home;
