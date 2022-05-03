b29bfe12d01d4c206b76c31ebfba6c01459348c0
payamrwd <payamrwd@gmail.com>
2022-05-03 20:34:10 +0430

    adding a product page (2)

--- pages/products/[pid].tsx
+++ pages/products/[pid].tsx
@@ -1,11 +1,20 @@
 import { useRouter } from 'next/router';
 import { NextPage } from 'next';
+import Layout from '../../components/layout';
+import Head from 'next/head';
 
 const Product: NextPage = () => {
   const router = useRouter();
   const { pid } = router.query;
 
-  return <p>Product {pid}!</p>;
+  return (
+    <Layout>
+      <Head>
+        <title>product {pid} page title</title>
+      </Head>
+      <h2>Product {pid}!</h2>
+    </Layout>
+  );
 };
 
 export default Product;
