2f47b70c41f86e9e0e3729a62e05d9f774f055fb
payamrwd <payamrwd@gmail.com>
2022-05-03 14:23:57 +0430

    loading index products using getStaticProps (4): finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import type { NextPage, GetStaticProps } from 'next';
 import Layout from '../components/layout';
 import Head from 'next/head';
-import styles from '../styles/index.module.scss';
+import Link from 'next/link';
+import {
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Button,
+  Box,
+} from '@mui/material';
 
 interface Product {
   id: number;
   title: string;
-  image: string;
+  imageURL: string;
   description: string;
 }
 
@@ -19,7 +29,7 @@ type Props = {
 type Product = {
   id: number;
   title: string;
-  image: string;
+  imageURL: string;
   description: string;
 }
 
@@ -34,6 +44,39 @@ const Home: NextPage<Props> = ({ products }: Props) => {
       <Head>
         <title>index page title</title>
       </Head>
+      <Container maxWidth={false}>
+        <Typography component="div">
+          <h2>Products to Consider!</h2>
+          {/* Products */}
+          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            {products.map((product) => (
+              /* Each Product */
+              <Card key={product.id} sx={{ maxWidth: 345, m: 2 }}>
+                <CardMedia
+                  component="img"
+                  height="240"
+                  image={'images/products/' + product.imageURL + '.jpg'}
+                  alt="001"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {product.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Add to Cart</Button>
+                  <Button size="small">
+                    <Link href={'/products/100' + product.id}>Learn More</Link>
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+          </Box>
+        </Typography>
+      </Container>
     </Layout>
   );
 };
