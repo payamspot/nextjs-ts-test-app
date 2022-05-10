c643672feea1c220d4bad991d24c91984429c6e4
payamrwd <payamrwd@gmail.com>
2022-05-10 12:29:44 +0430

    added dynamic blog posts! using client-side fetching method using axios + loading

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,63 @@
 import type { NextPage } from 'next';
 import Layout from '../components/layout';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { CircularProgress, Container } from '@mui/material';
+
+interface Product {
+  id: number;
+  title: string;
+  imageURL: string;
+  description: string;
+}
 
 const Blog: NextPage = () => {
-  return (
-    <Layout>
-      <Head>
-        <title>blog page title</title>
-      </Head>
-      <h2>blog page!</h2>
-    </Layout>
-  );
+  const [isLoading, setLoading] = useState(false);
+  const [blogPosts, setBlogPosts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('http://localhost:3333/products').then((res) => {
+      const bPosts = res.data;
+      setBlogPosts(bPosts);
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading)
+    return (
+      <Layout>
+        <Head>
+          <title>blog page title</title>
+        </Head>
+        <Container maxWidth={false}>
+          <h2>Blog Posts !?</h2>
+          <CircularProgress />
+          <br />
+          <br />
+        </Container>
+      </Layout>
+    );
+  else {
+    return (
+      <Layout>
+        <Head>
+          <title>blog page title</title>
+        </Head>
+        <Container maxWidth={false}>
+          <h2>Blog Posts !?</h2>
+          <ul>
+            {blogPosts.map((post) => (
+              <li key={post.id}>
+                {post.title} - {post.description}
+              </li>
+            ))}
+          </ul>
+        </Container>
+      </Layout>
+    );
+  }
 };
 
 export default Blog;
