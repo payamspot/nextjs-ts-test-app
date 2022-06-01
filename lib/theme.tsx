33635546518e992ff4d162bad267bc09242310f1
payamrwd <payamrwd@gmail.com>
2022-06-01 11:35:26 +0430

    added the theme

---
+++ lib/theme.tsx
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  // typescript interfaces for the theme (https://mui.com/material-ui/customization/theming/#custom-variables)
+}
+
+const theme = createTheme({});
+
+export default theme;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../lib/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
