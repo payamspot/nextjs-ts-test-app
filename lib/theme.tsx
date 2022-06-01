94c9212f1a13313f5887a3ecd45da62c365a345a
payamrwd <payamrwd@gmail.com>
2022-06-01 12:36:55 +0430

    added fa-IR locale to the theme

--- lib/theme.tsx
+++ lib/theme.tsx
@@ -1,11 +1,18 @@
 import { createTheme } from '@mui/material/styles';
+import { faIR } from '@mui/material/locale';
+import { Localization } from '@mui/material/locale';
 
 declare module '@mui/material/styles' {
-  // typescript interfaces for the theme (https://mui.com/material-ui/customization/theming/#custom-variables)
+  // typescript interfaces for the theme
+  interface ThemeOptions {
+    direction?: string;
+    faIR?: Localization;
+  }
 }
 
 const theme = createTheme({
   direction: 'rtl',
+  faIR,
 });
 
 export default theme;
