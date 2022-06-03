172a83c87912139e3aeeaebabae3cdc71593bca1
payamrwd <payamrwd@gmail.com>
2022-06-03 12:18:58 +0430

    temporary changes to check sth (4)

--- components/layout.tsx
+++ components/layout.tsx
@@ -35,7 +35,7 @@ function Layout({ children }: Props) {
             margin: '7px 0',
           }}
         >
-          footer! -- version 1.0.3 -- footer!
+          footer! -- version 1.0.4 -- footer!
         </footer>
       </div>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,22 +10,22 @@ import theme from '../lib/theme';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
+// import { prefixer } from 'stylis';
 
 // create rtl cache
 const cacheRtl = createCache({
   key: 'muirtl',
-  stylisPlugins: [prefixer, rtlPlugin],
+  stylisPlugins: [rtlPlugin],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    /*<CacheProvider value={cacheRtl}>*/
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-    /*</CacheProvider>*/
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
