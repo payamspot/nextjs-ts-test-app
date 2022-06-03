1eb33d26046774cadae54a407bbf2049149c1292
payamrwd <payamrwd@gmail.com>
2022-06-03 13:38:45 +0430

    for rtl to use jss (4): oh! jss is deprecated from mui 5!!!

--- components/layout.tsx
+++ components/layout.tsx
@@ -35,7 +35,7 @@ function Layout({ children }: Props) {
             margin: '7px 0',
           }}
         >
-          footer! -- version 1.2.0 -- footer!
+          footer! -- version 1.2.1 -- footer!
         </footer>
       </div>
     </div>
