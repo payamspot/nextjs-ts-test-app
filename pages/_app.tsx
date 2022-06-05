da6669d04adbaab7c436d942176db7edd9ac6b59
payamrwd <payamrwd@gmail.com>
2022-06-05 12:22:15 +0430

    very small change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,5 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../lib/theme';
